072c0942c7a931edc37dfcdbbeaaed98422a9e3b
Benoit Marchant <benoit@montagestudio.com>
2017-04-27 08:48:37 +0000

    Add component address

---
+++ ui/address.reel/address.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/address.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Address
+ * @extends Component
+ */
+exports.Address = Component.specialize(/** @lends Address# */ {
+    constructor: {
+        value: function Address() {
+            this.super();
+        }
+    }
+});
