0a166c0284b0f7f3a189ad30428a4a5ff3c829ec
Benoit Marchant <benoit@montagestudio.com>
2017-04-27 11:14:34 +0000

    Add component other-form

---
+++ ui/other-form.reel/other-form.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/other-form.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class OtherForm
+ * @extends Component
+ */
+exports.OtherForm = Component.specialize(/** @lends OtherForm# */ {
+    constructor: {
+        value: function OtherForm() {
+            this.super();
+        }
+    }
+});
